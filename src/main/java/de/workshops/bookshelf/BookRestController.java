19d49b5238b36026fdeb3de757c9c5b55391655c
Birgit Kratz <mail@birgitkratz.de>
2022-10-13 11:11:11 +0200

    implement controller method for fetching books, add some inut parameter validation

---
+++ src/main/java/de/workshops/bookshelf/BookRestController.java
@@ -0,0 +1,58 @@
+package de.workshops.bookshelf;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.PostConstruct;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.io.IOException;
+import java.util.List;
+
+@RestController
+@RequestMapping("/book")
+@Validated
+public class BookRestController {
+    private final ResourceLoader resourceLoader;
+    private final ObjectMapper mapper;
+
+    private List<Book> books;
+
+    public BookRestController(ResourceLoader resourceLoader, ObjectMapper mapper) {
+        this.resourceLoader = resourceLoader;
+        this.mapper = mapper;
+    }
+
+    @PostConstruct
+    void fillBookList () throws IOException {
+        final var resource = resourceLoader.getResource("classpath:books.json");
+        this.books = mapper.readValue(resource.getInputStream(), new TypeReference<>() {});
+    }
+
+    @GetMapping
+    List<Book> getAllBooks() {
+        return books;
+    }
+
+    @GetMapping("/{isbn}")
+    Book getBookByIsbn(@PathVariable @NotBlank @Size(min = 3) String isbn) {
+        return books.stream()
+                .filter(book -> isbn.equals(book.getIsbn()))
+                .findFirst()
+                .orElseThrow();
+    }
+
+    @GetMapping(params = "author")
+    List<Book> getBooksByAuthor(@RequestParam String author) {
+        return books.stream()
+                .filter(book -> book.getAuthor().startsWith(author))
+                .toList();
+    }
+}
--- src/main/java/de/workshops/bookshelf/BookshelfApplication.java
+++ src/main/java/de/workshops/bookshelf/BookshelfApplication.java
@@ -1,7 +1,16 @@
 package de.workshops.bookshelf;
 
+import com.fasterxml.jackson.databind.util.ClassUtil;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.boot.web.servlet.context.ServletWebServerApplicationContext;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class BookshelfApplication {
@@ -10,4 +19,18 @@ public class BookshelfApplication {
 		SpringApplication.run(BookshelfApplication.class, args);
 	}
 
+//	@Bean
+//	ApplicationListener<ApplicationReadyEvent> contextListener (ApplicationContext context) {
+//		return new ApplicationListener<ApplicationReadyEvent>() {
+//			@Override
+//			public void onApplicationEvent(ApplicationReadyEvent event) {
+//				final var beanDefinitionNames = context.getBeanDefinitionNames();
+//				Arrays.stream(beanDefinitionNames).forEach(name -> System.out.println(name));
+//			}
+//		};
+//		return event -> {
+//			final var beanDefinitionNames = context.getBeanDefinitionNames();
+//			Arrays.stream(beanDefinitionNames).forEach(name -> System.out.println(name));
+//		};
+//	}
 }
