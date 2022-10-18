1b3a3885254455df35bec9acfd1d459c5cddeed8
Birgit Kratz <mail@birgitkratz.de>
2022-10-19 01:46:56 +0200

    chapter: database, activate Liquibase

--- src/test/java/de/workshops/bookshelf/BookJdbcRepositoryTest.java
+++ src/test/java/de/workshops/bookshelf/BookJdbcRepositoryTest.java
@@ -1,11 +1,9 @@
 package de.workshops.bookshelf;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
-import org.springframework.jdbc.core.JdbcTemplate;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
